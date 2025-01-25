e5937321b89dc1b25edad07182f0887d58897303
dmitry <iamdmitryvolkov@gmail.com>
2025-01-25 22:08:19 +0700

    lint

--- app/Console/Commands/DebugCommand.php
+++ app/Console/Commands/DebugCommand.php
@@ -3,13 +3,13 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use JetBrains\PhpStorm\NoReturn;
 
 class DebugCommand extends Command
 {
     protected $signature = 'debug';
     protected $description = 'Command description';
-    #[NoReturn] public function handle(): void
+
+    public function handle()
     {
         dd('ok');
     }
